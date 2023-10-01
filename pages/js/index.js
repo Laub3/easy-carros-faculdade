8fbe67014b8823177db4c82fbd25ce4a538ae92a
Laube 😎 <lucas.laube.f@gmail.com>
2023-10-01 20:26:52 -0300

    feat: auto selector screen

--- pages/js/index.js
+++ pages/js/index.js
@@ -3,6 +3,11 @@ window.onload = function() {
     dia.value = 0;
     const hora = document.getElementById('hora');
     hora.value = 1;
-  }
-  
+  },
+
+function redirectToLogin() {
+  document.getElementById("redirectToLogin").onclick = function () {
+    location.href = "/index.html";
+  };
+}
   
